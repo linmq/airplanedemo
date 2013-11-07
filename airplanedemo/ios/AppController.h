0180006b626118554dc37cfb488b4d46d3d431c3
Leo <leolmq@gmail.com>
2013-11-08 07:04:46 +0800

    Initial Commit

---
+++ airplanedemo/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  airplanedemoAppController.h
+//  airplanedemo
+//
+//  Created by Leo Lin on 11/8/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
